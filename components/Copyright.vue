76952d7606f265222352fc037b3fd18fbcc93d8f
Mantai59 <mmantaibusiness@gmail.com>
2023-06-10 17:27:18 +0200

    chore: footer styling + copyright component

---
+++ components/Copyright.vue
@@ -0,0 +1,31 @@
+<script>
+    export default {
+        data() {
+            return {
+                startYear: 2023,
+                copyrightString: "Copyright &copy;"
+            }
+        },
+        methods: {
+            createCopyrightString(startYear)
+            {
+                let res = "&copy;";
+                let currentYear = new Date().getFullYear();
+                if(currentYear == startYear)
+                {
+                    res += startYear;
+                }
+                else
+                {
+                    res += startyear + " - " + currentYear;
+                }
+                return res + " Mathias Mantai";
+            }
+        }
+
+    }
+</script>
+
+<template>
+    <span class="rotate-[-90deg]" v-html="this.createCopyrightString(2023)"></span>
+</template>
--- components/footer.vue
+++ components/footer.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="grid grid-cols-3 gap-4 sm:gap-0 justify-center sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:fixed sm:bottom-[0vw] sm:left-[5vw] sm:text-center sm:space-x-0 sm:m-0 sm:w-fit">
+    <div class="grid grid-cols-3 gap-4 justify-center 
+    sm:gap-0 sm:flex sm:flex-col sm:items-center sm:space-y-4 m:fixed 
+    sm:bottom-[0vw] sm:left-[5vw] sm:text-center sm:space-x-0 sm:m-0 sm:w-fit
+    fixed bottom-0 justify-center items-center
+    ">
         <a href="https://github.com/mathiasmantai" target="_blank"><Github /></a>
         <a href="https://www.linkedin.com/in/mathias-mantai-939a26238/" target="_blank"><Linkedin /></a>
         <a href="https://xing.com/profile/Mathias_Mantai/cv" target="_blank"><Xing /></a>
-        <div class="collapse sm:visible bg-[var(--textColorMain)] sm:w-[2px] sm:h-[50px]"></div>
+        <div class="collapse sm:visible bg-[var(--textColorNav)] sm:w-[2px] sm:h-[50px]"></div>
+    </div>
+    <div class="text-sm sm:fixed sm:bottom-[16vw] sm:left-[1.5vw] rotate-[-90deg]">
+        <Copyright />
     </div>
 </template>
