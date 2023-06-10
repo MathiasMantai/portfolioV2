5f68df2faed09a7e8788e894cde754764904872c
Mantai59 <mmantaibusiness@gmail.com>
2023-06-10 18:33:58 +0200

    chore: icon update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,11 @@ export default defineNuxtConfig({
         autoprefixer: {},
       },
   },
+  app: {
+    head: {
+      link: [
+        {rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg'}
+      ]
+    }
+  }
 })
