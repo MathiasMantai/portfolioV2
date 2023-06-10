a057cdbd186fa8b476bc4aa62c00f2cee2c85594
Mantai59 <mmantaibusiness@gmail.com>
2023-06-10 18:42:21 +0200

    fix: footer fix for mobile

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="grid grid-cols-3 gap-4 justify-center left-[calc(50%-52px)] fixed bottom-[1vw] items-center
-    sm:gap-0 sm:flex sm:flex-col sm:items-center sm:space-y-4 m:fixed 
+    <div class="flex gap-2 align-center justify-center left-[calc(50%-52px)] bottom-[1vw] items-center
+    sm:gap-0 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:fixed 
     sm:bottom-[0vw] sm:left-[71px] sm:text-center sm:space-x-0 sm:m-0 sm:w-fit
     ">
         <a href="https://github.com/mathiasmantai" target="_blank"><Github /></a>
@@ -9,7 +9,7 @@
         <div class="collapse sm:visible bg-[var(--textColorNav)] sm:w-[2px] sm:h-[50px]"></div>
     </div>
     <div class="
-    text-sm fixed bottom-0 w-full text-center
+    text-sm bottom-0 w-full text-center
     sm:w-fit sm:fixed sm:bottom-[280px] sm:left-[12px] sm:rotate-[-90deg]
     ">
         <Copyright />
