7196edea5fb72e699138fb179653e1e6fa48a1da
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-05 10:10:16 +0200

    feat: custom code highlighting component

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bg-[#060B16] pt-2 pb-2 relative rounded-sm mt-4">
+        <span v-if="languageText" :class="`absolute top-0 right-0 p-2 text-['${languageColor}']`">
+            {{ languageText }}
+        </span>
+        <slot />
+        <div class="absolute bottom-0 right-0 p-2">
+            <span v-if="copied" class="mb-4">
+                Code Copied
+            </span>
+            <button @click=copy(code)><Copy /></button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { useClipboard } from '@vueuse/core'
+
+    const props = withDefaults(
+        defineProps<{
+            code?: string,
+            language?: string | null,
+            filename?: string | null,
+            highlights?: any
+        }>(),
+        {
+            code: '',
+            language: null,
+            filename: null,
+            highlights: []
+        }
+    )
+
+    const languages: {[key: string] : {language: string, textColor: string}} = {
+        'js': {
+            language: 'js',
+            textColor: '#000'
+        },
+        'vue': {
+            language: 'vue',
+            textColor: '#000'
+        },
+    }
+
+    const languageText = computed(() => languages[props.language!].language ? languages[props.language!].language : null)
+    const languageColor = computed(() => languages[props.language!].textColor ? languages[props.language!].textColor : null)
+
+    const { copy, copied} = useClipboard()
+</script>
