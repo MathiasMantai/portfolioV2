9c867f5287bbb4e3201875d0a0ee1f6642554b3b
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-06 16:09:10 +0200

    fix: adding css to page transition

--- app.vue
+++ app.vue
@@ -2,9 +2,22 @@
     <div class="h-[100dvh] flex flex-col items-center sm:w-[70dvw] w-[100dvw] mx-auto">
         <Navigation />
         <NuxtPage class="py-8" :transition="{
-            name: 'bounce',
+            name: 'page',
             mode: 'out-in'
         }" />
         <Footer />
     </div>
 </template>
+
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
