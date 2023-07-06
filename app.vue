e46efbb71c779d5ed254c2e08610ffc3a156ffb5
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-06 16:07:15 +0200

    feat: adding page transition

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="h-[100dvh] flex flex-col items-center sm:w-[70dvw] w-[100dvw] mx-auto">
         <Navigation />
-        <NuxtPage class="py-8" />
+        <NuxtPage class="py-8" :transition="{
+            name: 'bounce',
+            mode: 'out-in'
+        }" />
         <Footer />
     </div>
 </template>
