dfb7dc323e6692ffa4b1b2e8a47e217ccaf0c861
mathiasmantai <mmantaibusiness@gmail.com>
2023-07-19 16:26:52 +0200

    chore: updating time formatting for blog post cards

--- util/index.ts
+++ util/index.ts
@@ -1,21 +1,21 @@
 export const convertDate = (published: string): string => {
     const months: { [key: number]: string } = {
-        1: "Jan",
-        2: "Feb",
-        3: "Mar",
-        4: "Apr",
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
         5: "May",
-        6: "Jun",
-        7: "Jul",
-        8: "Aug",
-        9: "Sep",
-        10: "Oct",
-        11: "Nov",
-        12: "Dec",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
     };
     const date = published.substring(0, 10);
     const [year, month, day] = date.split("-");
-    return `${day}-${months[parseInt(month)]}-${year}`;
+    return `${day}. ${months[parseInt(month)]}, ${year}`;
 };
 
 
