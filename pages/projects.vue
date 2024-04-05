691466d8ec2f1728765d296d3d2e14f691b008db
MathiasMantai <mmantaibusiness@gmail.com>
2024-04-05 16:12:00 +0200

    refactor: - splitted up apps and packages for project page - added download links for project with downloadable files - added download link for desktop cleanup

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,30 +1,57 @@
 <template>
-    <main class="sm:w-[60dvw] w-[100dvw] p-4 grow">
-        <p class="text-lg mb-8">
-            These are some of the projects/packages I have worked on /I am working on.
-        </p>
-        <div class="grid sm:grid-cols-2 grid-cols-1 gap-8">
-        <div v-for="project in projects" class="">
-            <ProjectCard :name="project.name" :description="project.description" :link="project.link" />
+  <main class="sm:w-[60dvw] w-[100dvw] p-4 grow">
+    <p class="text-lg mb-8">
+      These are some of the apps/packages I am working on.
+    </p>
+
+    <div>
+      <h2 class="text-2xl text-white">Apps</h2>
+      <hr class="bg-slate-700 border border-slate-700 mt-4" />
+      <div class="grid sm:grid-cols-2 grid-cols-1 gap-8 mt-4">
+        <div v-for="app in apps">
+          <ProjectCard
+            :name="app.name"
+            :description="app.description"
+            :link="app.link"
+            :download="app.download ?? ''"
+          />
         </div>
+      </div>
+      <br />
+      <h2 class="text-2xl text-white">Packages</h2>
+      <hr class="bg-slate-700 border border-slate-700 mt-4" />
+      <div class="grid sm:grid-cols-2 grid-cols-1 gap-8 mt-4">
+        <div v-for="pkg in packages">
+          <ProjectCard
+            :name="pkg.name"
+            :description="pkg.description"
+            :link="pkg.link"
+            :download="pkg.download ?? ''"
+          />
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
-    import projects from '../content/projects/projects.json'
+import projects from "../content/projects/projects.json";
 
-    useHead({
-        title: 'Mathias Mantai - Projects',
-        meta: [
-            {
-                name: 'keywords',
-                content: 'mathias, mantai, blog, fullstack, web, development, nuxt, vue, projects, portfolio, about, projects'
-            },
-            {
-                name: 'date', 
-                content: Date.now()
-            }
-        ]
-    })
+const apps = ref(projects.apps);
+const packages = ref(projects.packages);
+
+useHead({
+  title: "Mathias Mantai - Projects",
+  meta: [
+    {
+      name: "keywords",
+      content:
+        "mathias, mantai, blog, fullstack, web, development, nuxt, vue, projects, portfolio, about, projects",
+    },
+    {
+      name: "date",
+      content: Date.now(),
+    },
+  ],
+});
 </script>
