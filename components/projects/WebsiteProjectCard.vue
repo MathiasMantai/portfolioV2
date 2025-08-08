66fe0b4064d30eb80bdc4885dec618d1d8d0b75a
MathiasMantai <mmantaibusiness@gmail.com>
2025-08-08 23:44:20 +0200

    language fix

--- components/projects/WebsiteProjectCard.vue
+++ components/projects/WebsiteProjectCard.vue
@@ -24,7 +24,7 @@
         {{ description }}
       </p>
       <div class="flex items-center gap-2 mt-4 text-sm text-gray-400 group-hover:text-[var(--linkColor)]">
-        <span>Besuchen</span>
+        <span>Visit</span>
         <!-- Ein kleines Icon, das anzeigt, dass es ein externer Link ist -->
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
       </div>
